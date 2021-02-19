bc391000f57fa044542d75c25c642d792551c981
alexandre <alexandre.caila@ysocorp.com>
2021-02-19 21:07:24 +0100

    [WIP] noise

---
+++ Assets/src/Scripts/Game.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Yro {
+    public class Game : MonoBehaviour {
+        public enum State {
+            None,
+            Home,
+            Playing,
+            Pause,
+            Win,
+            Loose
+        }
+
+        private State _state;
+
+        public State state {
+            get => this._state;
+            set {
+                this._state = value;
+                if (this._state == State.Home) {
+                    if (SceneManager.GetSceneByName("Game").isLoaded) {
+                        StartCoroutine(this.UnloadScene());
+                    }
+                } else if (this._state == State.Playing) {
+                    if (!SceneManager.GetSceneByName("Game").isLoaded) {
+                        StartCoroutine(this.LoadScene());
+                    }
+                } else if (this._state == State.Pause) {
+
+                } else if (this._state == State.Win) {
+
+                } else if (this._state == State.Loose) {
+
+                }
+            }
+        }
+
+        private IEnumerator LoadScene() {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game", LoadSceneMode.Additive);
+            while (!asyncLoad.isDone) {
+                yield return null;
+            }
+        }
+
+        private IEnumerator UnloadScene() {
+            AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync("Game", UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+            while (!asyncLoad.isDone) {
+                yield return null;
+            }
+        }
+
+    }
+}
--- Assets/src/Scripts/Noise.cs
+++ Assets/src/Scripts/Noise.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Random = System.Random;
 using UnityEngine;
 
-public class Noise : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public static class Noise {
+    // https://www.youtube.com/watch?v=wbpMiKiSKm8&list=PLFt_AvWsXl0eBW2EiBtl_sxmDtSgZBxB3
+    public static float[,]  GenerateNoiseMap(int width, int height, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+        float[,] noiseMap = new float[height, width];
+        float amplitude = 1.0f;
+        float frequency = 1.0f;
+        float noiseHeight = .0f;
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+        float halfWidth = (float)width / 2.0f;
+        float halfHeight = (float)height / 2.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Random prng = new Random(seed);
+        Vector2[] octavesOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++) {
+            float xOffset = prng.Next(-100000, 100000) + offset.x;
+            float yOffset = prng.Next(-100000, 100000) + offset.y;
+            octavesOffsets[i] = new Vector2(xOffset, yOffset);
+        }
+
+        if (scale <= 0)
+            scale = .00001f;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                amplitude = 1.0f;
+                frequency = 1.0f;
+                noiseHeight = .0f;
+                for (int i = 0; i < octaves; i++) {
+                    float xSample = (x - halfWidth) / scale * frequency + octavesOffsets[i].x;
+                    float ySample = (y - halfHeight) / scale * frequency + octavesOffsets[i].y;
+                    noiseHeight *= (Mathf.PerlinNoise(xSample, ySample) * 2 - 1) * amplitude;
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                    maxNoiseHeight = noiseHeight;
+                else if (noiseHeight < minNoiseHeight)
+                    minNoiseHeight = noiseHeight;
+
+                noiseMap[y, x] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                noiseMap[y, x] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[y, x]);
+            }
+        }
+        return noiseMap;
     }
 }
