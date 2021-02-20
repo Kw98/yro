05807fb137e131acff26a53fb6dfb102eaf54a94
alexandre <alexandrecaila26@gmail.com>
2021-02-20 16:07:12 +0100

    [WIP] movements

--- Assets/src/Scripts/PlayerControls.cs
+++ Assets/src/Scripts/PlayerControls.cs
@@ -3,27 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Yro {
-    public class PlayerControls : YrBehavior {
+    public class PlayerControls : MonoBehaviour {
         public float speed;
         private Rigidbody _rigidbody;
         private void Start()
         {
-            this._rigidbody = this.player.GetComponent<Rigidbody>();
+            this._rigidbody = this.GetComponent<Rigidbody>();
         }
 
-        private void FixedUpdate() {
-            if (Input.GetKeyDown(KeyCode.Z)) {
-                this._rigidbody.MovePosition(this.player.transform.position + (Vector3.forward * this.speed * Time.fixedDeltaTime));
-            } else if (Input.GetKeyDown(KeyCode.S))
+
+        //private void Update()
+        //{
+        //    Vector3 dir = Vector3.zero;
+        //    if (Input.GetKey("z")) {
+        //        dir = Vector3.forward;
+        //    } else if (Input.GetKey("s")) {
+        //        dir = Vector3.back;
+        //    }
+        //    if (Input.GetKey("q"))
+        //    {
+        //        dir += Vector3.left;
+        //    } else if (Input.GetKey("d")) {
+        //        dir += Vector3.right;
+        //    }
+        //    this.transform.position += (dir * this.speed * Time.deltaTime);
+        //}
+
+        private void FixedUpdate()
+        {
+            if (Input.GetKey(KeyCode.Z))
             {
-                this._rigidbody.MovePosition(this.player.transform.position + (Vector3.back * this.speed * Time.fixedDeltaTime));
+                this._rigidbody.MovePosition(this.transform.position + (Vector3.forward * this.speed * Time.fixedDeltaTime));
             }
-            if (Input.GetKeyDown(KeyCode.Q))
+            else if (Input.GetKey(KeyCode.S))
             {
-                this._rigidbody.MovePosition(this.player.transform.position + (Vector3.left * this.speed * Time.fixedDeltaTime));
-            } else if (Input.GetKeyDown(KeyCode.D))
+                this._rigidbody.MovePosition(this.transform.position + (Vector3.back * this.speed * Time.fixedDeltaTime));
+            }
+            if (Input.GetKey(KeyCode.Q))
             {
-                this._rigidbody.MovePosition(this.player.transform.position + (Vector3.right * this.speed * Time.fixedDeltaTime));
+                this._rigidbody.MovePosition(this.transform.position + (Vector3.left * this.speed * Time.fixedDeltaTime));
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                this._rigidbody.MovePosition(this.transform.position + (Vector3.right * this.speed * Time.fixedDeltaTime));
             }
         }
     }
