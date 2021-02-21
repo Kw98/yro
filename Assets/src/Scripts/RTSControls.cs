c9b403729f99e0cd470f6a12ffc784991ed6f352
alexandre caila <alexandre.caila@epitech.eu>
2021-02-21 22:45:09 +0100

    [WIP] custom rotation for rts like movements

--- Assets/src/Scripts/RTSControls.cs
+++ Assets/src/Scripts/RTSControls.cs
@@ -6,10 +6,14 @@ using UnityEngine.AI;
 namespace Yro
 {
     public class RTSControls : YBehaviour {
+        [SerializeField] private float _attackRange = 5;
+        [SerializeField, Range(0.0001f, 1.0f)] private float _smoothRotation = 0.2f;
         private NavMeshAgent _agent;
+        private Transform _lastHit;
 
         private void Awake() {
             this._agent = this.GetComponentInChildren<NavMeshAgent>();
+            this._agent.updateRotation = false;
         }
 
         void Update() {
@@ -18,11 +22,27 @@ namespace Yro
                 if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue)) {
                     this._agent.destination = hit.point;
                     this._agent.isStopped = false;
+                    this._lastHit = hit.transform;
                 }
+            }
+            if (this._lastHit && this._lastHit.tag == "Enemy" && Vector3.Distance(this.transform.position, this._lastHit.position) <= this._attackRange) {
+                this._agent.isStopped = true;
+                this.transform.LookAt(this._lastHit);
+                this.transform.localEulerAngles = new Vector3(.0f, this.transform.localEulerAngles.y, .0f);
             }
             if (Input.GetKeyDown(KeyCode.S)) {
                 this._agent.isStopped = true;
             }
+        }
+
+        private void LateUpdate() {
+            if (!this._agent.isStopped && this._agent.velocity.sqrMagnitude > Mathf.Epsilon) {
+                Quaternion rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(this._agent.velocity.normalized), this._smoothRotation);
+                rotation.x = .0f;
+                rotation.z = .0f;
+                transform.rotation = rotation;
+            }
+
         }
     }
 }
